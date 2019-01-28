06798172bb3a2318c974c1cd623c47f0402b67de
Matthias Gutjahr <mattsches@gmail.com>
2019-01-28 11:59:03 +0100

    update Adminer to version 4.7.1
    also, remove Chinese version because we don't need it here

--- src/Http/Controllers/AdminerController.php
+++ src/Http/Controllers/AdminerController.php
@@ -15,24 +15,14 @@ class AdminerController extends Controller
         $this->middleware('adminer');
 
         // adminer version
-        $this->version = '4.6.3';
+        $this->version = '4.7.1';
         // default adminer
         $this->adminer = $this->getAdminerFileName();
     }
 
     public function index()
     {
-        $locale = strtolower(app()->getLocale());
-
-        // localization
-        switch ($locale) {
-            case 'zh-tw':
-            case 'zh-hant':
-                $this->adminer = $this->getAdminerFileName('zh-tw');
-                break;
-        }
-
-        require(__DIR__.'/../../../resources/'.$this->adminer);
+        require __DIR__.'/../../../resources/'.$this->adminer;
     }
 
     private function getAdminerFileName($locale = 'en')
